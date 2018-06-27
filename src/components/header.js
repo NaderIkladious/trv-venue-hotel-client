464e8811296c8425224c738aee477aabeefa2245
Nader Ikladious <nader.ikladious@gmail.com>
2018-06-27 09:04:37 +0200

    feat(ui): style header

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../logo.png';
 
 export default class Header extends React.Component {
   render() {
     return (
       <header className="header">
         <div className="wrapper">
-          <h1>Trivago Venue Hotel</h1>
+          <Link to="/" title="Trivago Venue Hotel">
+            <img src={logo} alt="Trivago Venue Hotel Logo" height="70px" />
+          </Link>
         </div>
       </header>
     );
