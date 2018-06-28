d836f57528c6d85631b06553db988a3a49f3b2e8
Nader Ikladious <nader.ikladious@gmail.com>
2018-06-28 18:55:23 +0200

    feat: add current logged in user in header

--- src/components/header.js
+++ src/components/header.js
@@ -10,6 +10,12 @@ export default class Header extends React.Component {
           <Link to="/" title="Trivago Venue Hotel">
             <img src={logo} alt="Trivago Venue Hotel Logo" height="70px" />
           </Link>
+          <div className="user">
+            <span>John Doe</span>
+            <div className="avatar">
+              <img src="./images/avatar.png" alt="User Avatar" />
+            </div>
+          </div>
         </div>
       </header>
     );
