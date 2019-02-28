ea2e53519b231d5f76359d23b7d50bbd88d24288
Nader Ikladious <nader.ikladious@gmail.com>
2019-02-28 10:10:52 +0200

    fix(refactor): confirmation logic refactor

--- src/containers/confirmation/index.js
+++ src/containers/confirmation/index.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import axios from 'axios';
 import { Helmet } from 'react-helmet';
 
+import { Spinner } from '../../components';
 import { API_URL } from '../../core/consts';
 
 import './style.css';
@@ -14,37 +15,39 @@ export class Confirmation extends React.Component {
   componentDidMount() {
     // Get confirmation ID from the url
     const confirmationId = this.props.match.params.id;
-    if (this.props.location && this.props.location.state && this.props.location.state.room) {
-      const { room, hotel } = this.props.location.state;
-      if (_.has(localStorage, confirmationId)) {
-        // If confirmation ID exists in the local storage set it to state
-        this.setState({ confirmation: JSON.parse(localStorage[confirmationId]) });
-      } else {
-        // Otherwise create new confirmation with API POST with details
-        const confirmation = {
-          id: confirmationId,
-          roomName: room.name,
-          firstName: 'John',
-          lastName: 'Doe',
-          hotelName: hotel.name,
-          price: room.price_in_usd,
-          date: new Date()
-        };
-        localStorage.setItem(confirmationId, JSON.stringify(confirmation));
-        this.setState({ confirmation });
-        axios.post(`${API_URL}/confirmations`, confirmation).then(res => {
-          console.log('posting data', res);
-        });
-      }
+    if (_.has(localStorage, confirmationId)) {
+      this.setState({ confirmation: JSON.parse(localStorage[confirmationId]) });
     } else {
-      if (_.has(localStorage, confirmationId)) {
-        this.setState({ confirmation: JSON.parse(localStorage[confirmationId]) });
-      } else {
-        axios.get(`${API_URL}/confirmations/${confirmationId}`).then(res => {
+      axios.get(`${API_URL}/confirmations/${confirmationId}`).then(
+        res => {
           console.log('getting data', res);
-          this.setState({ confirmation: res.data });
-        });
-      }
+          this.setState({ confirmation: res.data, loading: false });
+          localStorage.setItem(confirmationId, JSON.stringify(res.data));
+        },
+        err => {
+          if (this.props.location && this.props.location.state && this.props.location.state.room) {
+            console.log('have props');
+            const { room, hotel } = this.props.location.state;
+            const confirmation = {
+              id: confirmationId,
+              roomName: room.name,
+              firstName: 'John',
+              lastName: 'Doe',
+              hotelName: hotel.name,
+              price: room.price_in_usd,
+              date: new Date()
+            };
+            localStorage.setItem(confirmationId, JSON.stringify(confirmation));
+            this.setState({ confirmation, loading: false });
+            axios.post(`${API_URL}/confirmations`, confirmation).then(res => {
+              console.log('posting data', res);
+            });
+          } else {
+            console.log('no data');
+            this.setState({ loading: false });
+          }
+        }
+      );
     }
   }
   render() {
@@ -54,39 +57,51 @@ export class Confirmation extends React.Component {
         <Helmet>
           <title>{`Confirmation #${confirmation.id}`}</title>
         </Helmet>
-        <div className="wrapper">
-          <h3>Thanks for using Trivago</h3>
-          <div className="reservations-details">
-            <table>
-              <tbody>
-                <tr>
-                  <td>Confirmation ID</td>
-                  <td className="confirmation-id">{confirmation.id}</td>
-                </tr>
-                <tr>
-                  <td>Hotel Name</td>
-                  <td>{confirmation.hotelName}</td>
-                </tr>
-                <tr>
-                  <td>Room Name</td>
-                  <td>{confirmation.roomName}</td>
-                </tr>
-                <tr>
-                  <td>Price / Night</td>
-                  <td>${parseFloat(confirmation.price).toPrecision(5)}</td>
-                </tr>
-                <tr>
-                  <td>First Name</td>
-                  <td className="first-name">{confirmation.firstName}</td>
-                </tr>
-                <tr>
-                  <td>Last Name</td>
-                  <td className="last-name">{confirmation.lastName}</td>
-                </tr>
-              </tbody>
-            </table>
+        {this.state.loading ? (
+          <Spinner />
+        ) : (
+          <div>
+            {_.keys(confirmation).length > 0 ? (
+              <div className="wrapper">
+                <h3>Thanks for using Trivago</h3>
+                <div className="reservations-details">
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Confirmation ID</td>
+                        <td className="confirmation-id">{confirmation.id}</td>
+                      </tr>
+                      <tr>
+                        <td>Hotel Name</td>
+                        <td>{confirmation.hotelName}</td>
+                      </tr>
+                      <tr>
+                        <td>Room Name</td>
+                        <td>{confirmation.roomName}</td>
+                      </tr>
+                      <tr>
+                        <td>Price / Night</td>
+                        <td>${parseFloat(confirmation.price).toPrecision(5)}</td>
+                      </tr>
+                      <tr>
+                        <td>First Name</td>
+                        <td className="first-name">{confirmation.firstName}</td>
+                      </tr>
+                      <tr>
+                        <td>Last Name</td>
+                        <td className="last-name">{confirmation.lastName}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            ) : (
+              <div className="no-result">
+                <p>Reservation not found.</p>
+              </div>
+            )}
           </div>
-        </div>
+        )}
       </div>
     );
   }
